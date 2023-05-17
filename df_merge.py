ece5897f0b9e295c2bd43aec06f7048729542a7f
saeuggang10 <78424269+saeuggang10@users.noreply.github.com>
2023-05-17 16:18:27 +0900

    Update df_merge.py

--- df_merge.py
+++ df_merge.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # 데이터 다루기 _ 총정리
+# # 데이터 다루기 _ 총정리 ##
 # - 데이터 수집
 # - 데이터 읽어들이기
 # - 기준년도 컬럼추가 (2019년 1월 ~ 2022년 1월)
@@ -10,13 +10,14 @@
 
 # In[ ]:
 
-
+#기준년도 리스트 만들기
 date_temp = []
 for y in ['2019', '2020', '2021']:
     for m in range(1,13):
         date_temp.append(y+'-'+str(m).zfill(2))
 date_temp = date_temp + ['2022-01']
 
+#데이터 읽어들이고 합치기
 path = 'C:/edu_busan_202305/02_데이터처리/08_day/files2/'
 df_all2 = pd.DataFrame()
 df_code = pd.read_excel('sample_codemaster.xlsx')
